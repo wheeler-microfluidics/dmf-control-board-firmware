1b8e30d6abef0a97870ebb6b82d88d0ae86aca18
Ryan Fobel <ryan@fobel.net>
2011-11-01 21:14:47 -0400

    Add feedback code and implement more functionality as a plugin
    The DmfControlBoard() class is now created from within the plugin
    and calls to set the state and measure impedance are handled through
    the plugin API.
    
    Also fix the License section on some of files.

--- avr/__init__.py
+++ avr/__init__.py
@@ -1,20 +1,20 @@
 """
-Copyright 2011 Ryan Fobel and Christian Fobel
+Copyright 2011 Ryan Fobel
 
-This file is part of Microdrop.
+This file is part of dmf_control_board.
 
-Microdrop is free software: you can redistribute it and/or modify
+dmf_control_board is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Microdrop is distributed in the hope that it will be useful,
+dmf_control_board is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Microdrop.  If not, see <http://www.gnu.org/licenses/>.
+along with dmf_control_board.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys
