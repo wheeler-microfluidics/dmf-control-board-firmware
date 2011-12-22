4af16db3a4707bc7542e9ddeb3234910bda6081e
Ryan Fobel <ryan@fobel.net>
2011-12-22 17:33:40 -0500

    Add EEPROM address definitions to python module

--- src/python_wrappers.cpp
+++ src/python_wrappers.cpp
@@ -35,6 +35,10 @@ const uint8_t RemoteObject::RETURN_BAD_PACKET_SIZE;
 const uint8_t RemoteObject::RETURN_BAD_CRC;
 const uint8_t DmfControlBoard::SINE;
 const uint8_t DmfControlBoard::SQUARE;
+const uint16_t DmfControlBoard::EEPROM_INIT;
+const uint16_t DmfControlBoard::EEPROM_POT_WAVEOUT_GAIN_1;
+const uint16_t DmfControlBoard::EEPROM_AREF;
+const uint16_t DmfControlBoard::EEPROM_VGND;
 
 BOOST_PYTHON_MODULE(dmf_control_board_base)
 {
@@ -124,4 +128,8 @@ DmfControlBoard_class.attr("RETURN_NOT_CONNECTED") = DmfControlBoard::RETURN_NOT
 DmfControlBoard_class.attr("RETURN_BAD_INDEX") = DmfControlBoard::RETURN_BAD_INDEX;
 DmfControlBoard_class.attr("RETURN_BAD_PACKET_SIZE") = DmfControlBoard::RETURN_BAD_PACKET_SIZE;
 DmfControlBoard_class.attr("RETURN_BAD_CRC") = DmfControlBoard::RETURN_BAD_CRC;
+DmfControlBoard_class.attr("EEPROM_INIT") = DmfControlBoard::EEPROM_INIT;
+DmfControlBoard_class.attr("EEPROM_POT_WAVEOUT_GAIN_1") = DmfControlBoard::EEPROM_POT_WAVEOUT_GAIN_1;
+DmfControlBoard_class.attr("EEPROM_AREF") = DmfControlBoard::EEPROM_AREF;
+DmfControlBoard_class.attr("EEPROM_VGND") = DmfControlBoard::EEPROM_VGND;
 }
