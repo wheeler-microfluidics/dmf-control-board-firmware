d0eda40bdee5edec1cb5aa2f1143dad398a42c45
Christian Fobel <christian@fobel.net>
2012-04-07 04:32:29 -0400

    Refactor step options management into StepOptio...
    Refactor common step options management into StepOptionsController class.

--- microdrop/__init__.py
+++ microdrop/__init__.py
@@ -41,6 +41,7 @@ from flatland.validation import ValueAtLeast, ValueAtMost
 from logger import logger
 from pygtkhelpers.ui.objectlist import PropertyMapper
 from gui.protocol_grid_controller import ProtocolGridController
+from plugin_helpers import StepOptionsController
 from plugin_manager import IPlugin, IWaveformGenerator, Plugin, \
     implements, PluginGlobals, ScheduleRequest, emit_signal,\
             get_service_instance
@@ -91,14 +92,14 @@ def format_func(value):
         return False
 
 
-class DmfControlBoardPlugin(Plugin):
+class DmfControlBoardPlugin(Plugin, StepOptionsController):
     """
     This class is automatically registered with the PluginManager.
     """
     implements(IPlugin)
     implements(IWaveformGenerator)
 
-    Fields = Form.of(
+    StepFields = Form.of(
         Integer.named('duration').using(default=100, optional=True,
             validators=[ValueAtLeast(minimum=0), ]),
         Integer.named('voltage').using(default=100, optional=True,
@@ -432,41 +433,14 @@ class DmfControlBoardPlugin(Plugin):
         """
         self.control_board.set_waveform_frequency(frequency)
 
-    def get_default_options(self):
+    def get_default_step_options(self):
         return DmfControlBoardOptions()
 
-    def get_step(self, default):
-        if default is None:
-            return get_app().protocol.current_step_number
-        return default
-
-    def get_step_options(self, step_number=None):
-        """
-        Return a DmfControlBoardOptions object for the current step in the
-        protocol.  If none exists yet, create a new one.
-        """
-        step_number = self.get_step(step_number)
-        app = get_app()
-        
-        step = app.protocol.steps[step_number]
-        options = step.get_data(self.name)
-        if options is None:
-            # No data is registered for this plugin (for this step).
-            options = self.get_default_options()
-            step.set_data(self.name, options)
-        return options
-
-    def get_step_form_class(self):
-        return self.Fields
-
-    def get_step_fields(self):
-        return self.Fields.field_schema_mapping.keys()
-
     def set_step_values(self, values_dict, step_number=None):
-        step_number = self.get_step(step_number)
+        step_number = self.get_step_number(step_number)
         logger.debug('[DmfControlBoardPlugin] set_step[%d]_values(): '\
                     'values_dict=%s' % (step_number, values_dict,))
-        el = self.Fields(value=values_dict)
+        el = self.StepFields(value=values_dict)
         if not el.validate():
             raise ValueError('Invalid values: %s' % el.errors)
         options = self.get_step_options(step_number=step_number)
@@ -491,7 +465,7 @@ class DmfControlBoardPlugin(Plugin):
             return None
 
         values = {}
-        for name in self.Fields.field_schema_mapping:
+        for name in self.StepFields.field_schema_mapping:
             try:
                 value = getattr(options, name)
             except AttributeError:
@@ -501,7 +475,7 @@ class DmfControlBoardPlugin(Plugin):
 
     def get_step_value(self, name, step_number=None):
         app = get_app()
-        if not name in self.Fields.field_schema_mapping:
+        if not name in self.StepFields.field_schema_mapping:
             raise KeyError('No field with name %s for plugin %s' % (name, self.name))
         if step_number is None:
             step_number = app.protocol.current_step_number
