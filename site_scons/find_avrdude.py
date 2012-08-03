wheeler-microfluidics/dmf-control-board-firmware

commit 8e4456f96f8c505d9f3409443c19a9a135740069

Use arduino version 0023 by default
This is a temporary workaround that allows users to have both 0023 and newer
1.x arduino folders in their path. The scons script will only work with 0023
for now. See #12.

site_scons/find_avrdude.py
@@ -1,6 +1,8 @@
+import re
 import sys
 import os
 from itertools import chain
+
 from path import path
 
 home_dir = path('~').expand()
@@ -30,9 +32,12 @@ def get_arduino_paths():
     Searched:
         %s''' % '\n    '.join(ARDUINO_SEARCH_PATHS)
         sys.exit(1)
-    fs.sort(key=lambda x: -x.ctime)
 
-    avrdude = fs[0]
+    # use arduino version 0023 if it exists
+    for avrdude in fs:
+        if get_arduino_version(avrdude)=='0023':
+            break
+
     p = avrdude.parent
     while p and p.name != 'hardware':
         p = p.parent
@@ -51,6 +56,22 @@ def get_arduino_paths():
 
 def get_avrdude_list(p):
     return list(set(chain(*[d.walkfiles(AVRDUDE_NAME) for d in p.dirs('arduino*')])))
+
+
+def get_arduino_version(p):
+    while p and not (p / path('revisions.txt')).exists():
+        p = p.parent
+    if not p:
+        print >> sys.stderr, '''Arduino install path not found.'''
+        sys.exit(1)
+    with open(p / path('revisions.txt'), 'r') as f:
+        version = f.readline()
+    f.close()
+    match = re.search(r'ARDUINO (.*) - .*', version)
+    if match:
+        return match.groups()[0]
+    else:
+        return None
 
 
 if __name__ == '__main__':

commit dc129853798eab2bfe13fed69e098d25b3bfafda

Fix driver compilation on Linux
The search for version 23 that was added in 8e4456f96f8c505d9f3409443c19a9a135740069
caused the build to fail on Linux.  Disable this search unless running on Windows.

site_scons/find_avrdude.py
@@ -33,10 +33,13 @@ def get_arduino_paths():
         %s''' % '\n    '.join(ARDUINO_SEARCH_PATHS)
         sys.exit(1)
 
-    # use arduino version 0023 if it exists
-    for avrdude in fs:
-        if get_arduino_version(avrdude)=='0023':
-            break
+    if os.name == 'nt':
+        # use arduino version 0023 if it exists
+        for avrdude in fs:
+            if get_arduino_version(avrdude)=='0023':
+                break
+    else:
+        avrdude = fs[0]
 
     p = avrdude.parent
     while p and p.name != 'hardware':