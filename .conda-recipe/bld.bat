602249c0e0fd5079e65c6ed4fa751562f4fb960c
Christian Fobel <christian@fobel.net>
2017-02-07 18:32:26 -0500

    [WIP] Add Conda recipe and PlatformIO config

---
+++ .conda-recipe/bld.bat
@@ -0,0 +1,42 @@
+@echo off
+set PKG_NAME=dmf-control-board-firmware
+setlocal enableextensions
+md "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v1_1
+md "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v1_2
+md "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v1_3
+md "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v2_0
+md "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v2_1
+endlocal
+
+REM Generate Arduino/Python code
+paver create_config
+
+REM Build firmware
+pio run
+if errorlevel 1 exit 1
+REM Copy compiled firmware to Conda bin directory
+copy "%SRC_DIR%"\.pioenvs\mega2560_hw_v1_1\firmware.hex "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v1_1\firmware.hex
+copy "%SRC_DIR%"\.pioenvs\mega2560_hw_v1_2\firmware.hex "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v1_2\firmware.hex
+copy "%SRC_DIR%"\.pioenvs\mega2560_hw_v1_3\firmware.hex "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v1_3\firmware.hex
+copy "%SRC_DIR%"\.pioenvs\mega2560_hw_v2_0\firmware.hex "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v2_0\firmware.hex
+copy "%SRC_DIR%"\.pioenvs\mega2560_hw_v2_1\firmware.hex "%PREFIX%"\Library\bin\platformio\%PKG_NAME%\mega2560_hw_v2_1\firmware.hex
+if errorlevel 1 exit 1
+
+REM Build Python C-extension
+scons
+
+REM Generate `setup.py` from `pavement.py` definition.
+"%PYTHON%" -m paver generate_setup
+
+REM **Workaround** `conda build` runs a copy of `setup.py` named
+REM `conda-build-script.py` with the recipe directory as the only argument.
+REM This causes paver to fail, since the recipe directory is not a valid paver
+REM task name.
+REM
+REM We can work around this by wrapping the original contents of `setup.py` in
+REM an `if` block to only execute during package installation.
+"%PYTHON%" -c "from __future__ import print_function; input_ = open('setup.py', 'r'); data = input_.read(); input_.close(); output_ = open('setup.py', 'w'); output_.write('\n'.join(['import sys', 'import path_helpers as ph', '''if ph.path(sys.argv[0]).name == 'conda-build-script.py':''', '    sys.argv.pop()', 'else:', '\n'.join([('    ' + d) for d in data.splitlines()])])); output_.close(); print(open('setup.py', 'r').read())"
+
+REM Install source directory as Python package.
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+if errorlevel 1 exit 1
