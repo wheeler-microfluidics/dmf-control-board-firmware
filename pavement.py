151b31d18f14976b1bc5beb27cedad584d13f23b
Christian Fobel <christian@fobel.net>
2017-02-21 16:10:25 -0500

    Set software version in Config.h

--- pavement.py
+++ pavement.py
@@ -1,10 +1,11 @@
 from pprint import pprint
+import os
+import re
+import subprocess as sp
+import sys
 
 from paver.easy import task, needs, path, sh, cmdopts, options
 from paver.setuputils import setup
-
-import os
-import sys
 
 # add the current directory as the first listing on the python path
 # so that we import the correct version.py
@@ -32,9 +33,23 @@ setup(name='wheeler.dmf-control-board-firmware',
 
 @task
 def create_config():
+    def get_version_string():
+        version = sp.check_output('git describe', shell=True)
+        branch = sp.check_output('git rev-parse --abbrev-ref HEAD', shell=True)
+        m = re.search('^v(?P<major>\d+)\.(?P<minor>\d+)(-(?P<micro>\d+))?(?P<tags>.*)',
+                      version)
+        if m.group('micro'):
+            micro = m.group('micro')
+        else:
+            micro = '0'
+        return "%s.%s.%s%s" % (m.group('major'), m.group('minor'), micro,
+                               m.group('tags'))
     sketch_directory = path('src')
-    sketch_directory.joinpath('Config.h.skeleton').copy(sketch_directory
-                                                        .joinpath('Config.h'))
+    source_data = sketch_directory.joinpath('Config.h.skeleton').bytes()
+    config_data = source_data.replace('#define ___SOFTWARE_VERSION___ "0.1.0"',
+                                      '#define ___SOFTWARE_VERSION___ "{}"'
+                                      .format(get_version_string()))
+    sketch_directory.joinpath('Config.h').write_bytes(config_data)
 
 
 @task
